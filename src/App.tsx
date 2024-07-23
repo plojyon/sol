2d95fb078cf34c7f2c7aaceed1b603db63f01e55
Yon Ploj <admin@yon.si>
2024-07-23 19:09:28 +0200

    Disable scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
     });
 
     return (
-        <div className="App" >
+        <div className="App">
             <Sky />
             <Geocentered>
                 <Atmosphere />
