f01a49adc6b53b0e1fbe6c067d90916b182327bb
Yon Ploj <admin@yon.si>
2024-07-23 18:51:26 +0200

    Fix rotation jump bug

--- src/redux/solarHour.tsx
+++ src/redux/solarHour.tsx
@@ -6,7 +6,17 @@ const initialState = { angle: -30 };
 const solarHourReducer = (state = initialState, action: AnyAction) => {
     switch (action.type) {
         case ACTIONS.SOLAR_HOUR_CHANGED: {
-            return { angle: action.payload };
+            const next = action.payload;
+            const prev = state.angle;
+            if (Math.abs(prev - next) > 180) {
+                if (prev < next) {
+                    return { angle: next - 360 };
+                }
+                else {
+                    return { angle: next + 360 };
+                }
+            }
+            return { angle: next };
         }
         default: {
             return state;
